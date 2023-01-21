f285061d08ec06a21b04a2d1bb6b577660d62569
David Daupeyroux <david.ddcreation@gmail.com>
2023-01-21 19:00:57 +0100

    feat: :sparkles: Finish score calculation

--- src/utils/score-calculators.spec.ts
+++ src/utils/score-calculators.spec.ts
@@ -1,44 +1,122 @@
 import { Frame } from '../models';
-import { frameScore, isFrameClosed } from './score-calculators';
-
-const emptyFrames: Frame[] = [];
-const spare: Frame[] = [
-  [2, 8],
-  [10, 0],
-];
-
-const strikes: Frame[] = [
-  [10, 0],
-  [10, 0],
-  [10, 0],
-];
-
-const normalPlay: Frame[] = [
-  [8, 1],
-  [10, 0],
-  [10, 0],
-];
+import {
+  frameScore,
+  isFrameClosed,
+  isSpare,
+  isStrike,
+} from './score-calculators';
 
 describe('The isFrameClosed function', () => {
   it('Should return false for spare', () => {
-    expect(isFrameClosed(spare[0])).toBe(false);
+    expect(isFrameClosed([2, 8])).toBe(false);
   });
 
   it('Should return false for strike', () => {
-    expect(isFrameClosed(strikes[0])).toBe(false);
+    expect(isFrameClosed([10, 0])).toBe(false);
   });
 
   it('Should return true for others', () => {
-    expect(isFrameClosed(normalPlay[0])).toBe(true);
+    expect(isFrameClosed([8, 1])).toBe(true);
+  });
+});
+
+describe('The isSpare function', () => {
+  it('Should return true for spare', () => {
+    expect(isSpare([5, 5])).toBe(true);
+  });
+
+  it('Should return false for strike', () => {
+    expect(isSpare([10, 0])).toBe(false);
+  });
+
+  it('Should return false for others', () => {
+    expect(isSpare([8, 1])).toBe(false);
+  });
+});
+
+describe('The isStrike function', () => {
+  it('Should return true for strike', () => {
+    expect(isStrike([10, 0])).toBe(true);
+  });
+
+  it('Should return false for spare', () => {
+    expect(isStrike([7, 3])).toBe(false);
+  });
+
+  it('Should return false for others', () => {
+    expect(isStrike([6, 0])).toBe(false);
   });
 });
 
 describe('The frameScore function', () => {
   it('Should return undefined if current frame is not played', () => {
-    expect(frameScore(emptyFrames, 1)).toBeUndefined();
+    expect(frameScore([[4, 3]], 1)).toBeUndefined();
   });
 
   it('Should return undefined if current frame is a strike/spare and the next frame is not played', () => {
-    expect(frameScore(spare, 1)).toBeUndefined();
+    expect(
+      frameScore(
+        [
+          [2, 8],
+          [10, 0],
+        ],
+        1
+      )
+    ).toBeUndefined();
+  });
+
+  it('Should return the score when spare followed by another roll', () => {
+    expect(
+      frameScore(
+        [
+          [8, 2],
+          [5, 3],
+        ],
+        0
+      )
+    ).toBe(15);
+  });
+
+  describe('When a strike is done', () => {
+    it('Should return undefined when there is no more roll', () => {
+      expect(frameScore([[10, 0]], 0)).toBeUndefined();
+    });
+
+    it('Should return undefined when there is a new strike', () => {
+      expect(
+        frameScore(
+          [
+            [10, 0],
+            [10, 0],
+          ],
+          0
+        )
+      ).toBeUndefined();
+    });
+
+    it('Should return the score when there is a two more strike', () => {
+      expect(
+        frameScore(
+          [
+            [10, 0],
+            [10, 0],
+            [10, 0],
+          ],
+          0
+        )
+      ).toBe(30);
+    });
+
+    it('Should return the score when there is a two more rolls', () => {
+      expect(
+        frameScore(
+          [
+            [10, 0],
+            [8, 1],
+          ],
+          0
+        )
+      ).toBe(19);
+    });
   });
 });
--- src/utils/score-calculators.ts
+++ src/utils/score-calculators.ts
@@ -5,6 +5,15 @@ export const isFrameClosed = (frame: Frame): boolean => {
   return frameScore < 10;
 };
 
+export const isSpare = (frame: Frame): boolean => {
+  const frameScore = frame[0] + frame[1];
+  return frameScore === 10 && frame[0] !== 10;
+};
+
+export const isStrike = (frame: Frame): boolean => {
+  return frame[0] === 10;
+};
+
 export const frameScore = (
   frames: Frame[],
   frameIndex: number
@@ -15,6 +24,22 @@ export const frameScore = (
 
     if (isFrameClosed(frames[frameIndex])) {
       score = currentScore;
+    }
+    // Spare and next roll
+    else if (isSpare(frames[frameIndex]) && frames[frameIndex + 1]) {
+      score = 10 + frames[frameIndex + 1][0];
+    }
+    // Strike !!!
+    else if (isStrike(frames[frameIndex])) {
+      if (frames[frameIndex + 1]) {
+        if (!isStrike(frames[frameIndex + 1])) {
+          score = 10 + frames[frameIndex + 1][0] + frames[frameIndex + 1][1];
+        }
+        // Second strike in a row !!!!!
+        else if (frames[frameIndex + 2]) {
+          score = 10 + 10 + frames[frameIndex + 2][0];
+        }
+      }
     }
   }
 
