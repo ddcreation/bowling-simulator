f3793a4c033d6bbfcc9581a4fe94785d7b883454
David Daupeyroux <david.ddcreation@gmail.com>
2023-01-21 17:49:11 +0100

    feat: :sparkles: Render rows with frames

--- src/models/game.ts
+++ src/models/game.ts
@@ -4,6 +4,12 @@ import { Player } from './player.interface';
 
 const templatesPath = `${__dirname}/../templates`;
 
+interface TemplateReplace {
+  placeholder: string;
+  value: string;
+  length?: number;
+}
+
 export class Game {
   public players: Player[];
 
@@ -16,6 +22,65 @@ export class Game {
   public printBoard(): void {
     this._printHeader();
     this._printBody();
+  }
+
+  private _generateFrameReplacement(
+    player: Player,
+    frameIndex: number
+  ): TemplateReplace[] {
+    const replaceIndex = frameIndex + 1;
+    let total = '';
+    let symbol1 = '';
+    let symbol2 = '';
+    let symbol3 = '';
+
+    const replacements = [
+      {
+        placeholder: `$frame${replaceIndex}Total`,
+        value: total.toString(),
+        length: 2,
+      },
+      {
+        placeholder: `$frame${replaceIndex}Symbol1`,
+        value: symbol1,
+        length: 1,
+      },
+      {
+        placeholder: `$frame${replaceIndex}Symbol2`,
+        value: symbol2,
+        length: 1,
+      },
+    ];
+
+    if (replaceIndex === 10) {
+      replacements.push({
+        placeholder: `$frame${replaceIndex}Symbol3`,
+        value: symbol3,
+        length: 1,
+      });
+    }
+
+    return replacements;
+  }
+
+  private _generateFramesReplacements(player: Player): TemplateReplace[] {
+    let replacements: TemplateReplace[] = [];
+    let total = 0;
+
+    for (let i = 0; i < 10; i++) {
+      replacements = [
+        ...replacements,
+        ...this._generateFrameReplacement(player, i),
+      ];
+    }
+
+    replacements.push({
+      placeholder: '$total',
+      value: total.toString(),
+      length: 3,
+    });
+
+    return replacements;
   }
 
   private _printBody(): void {
@@ -34,9 +99,10 @@ export class Game {
       encoding: 'utf8',
     });
 
-    const replacements = [
+    const replacements: TemplateReplace[] = [
       { placeholder: '$index', value: (index + 1).toString(), length: 1 },
       { placeholder: '$name', value: player.name, length: 27 },
+      ...this._generateFramesReplacements(player),
     ];
 
     const row = replacements.reduce((output, config) => {
--- src/utils/formated-print.ts
+++ src/utils/formated-print.ts
@@ -1,10 +1,12 @@
-export const printFormated = (text: string, length: number): string => {
+export const printFormated = (text: string, length?: number): string => {
   let formatedText = text;
 
-  if (text.length < length) {
-    formatedText = text.padEnd(length, ' ');
-  } else if (text.length > length) {
-    formatedText = text.substring(0, length - 3) + '...';
+  if (length) {
+    if (text.length < length) {
+      formatedText = text.padEnd(length, ' ');
+    } else if (text.length > length) {
+      formatedText = text.substring(0, length - 3) + '...';
+    }
   }
 
   return formatedText;
