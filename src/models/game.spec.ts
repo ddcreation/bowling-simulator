4e9eefff9f82c15221bd0132e71d67024cf521e4
David Daupeyroux <david.ddcreation@gmail.com>
2023-01-21 20:13:51 +0100

    feat: :sparkles: Visualize boards from mock

--- src/models/game.spec.ts
+++ src/models/game.spec.ts
@@ -1,21 +1,23 @@
+import { fakePlayers } from '../mocks';
 import { Game } from './game';
+
+jest.mock('../utils', () => ({
+  printFormated: jest.fn((text) => text),
+  frameScore: jest.fn(),
+}));
 
 describe('Game', () => {
   describe('When initialized', () => {
     it('Should set passed players', () => {
-      const game = new Game(['Jon', 'Who', 'Why']);
+      const game = new Game(fakePlayers);
 
-      expect(game.players.length).toBe(3);
-      expect(game.players).toEqual([
-        { frames: [], name: 'Jon' },
-        { frames: [], name: 'Who' },
-        { frames: [], name: 'Why' },
-      ]);
+      expect(game.players.length).toBe(fakePlayers.length);
+      expect(game.players).toEqual(fakePlayers);
     });
   });
 
   describe('When the board is printed', () => {
-    const game = new Game(['Jon', 'Who']);
+    const game = new Game(fakePlayers);
 
     it('Should print header and body', () => {
       const printHeaderSpy = jest.spyOn(game, '_printHeader' as any);
