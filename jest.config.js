0613501f679b7eb39502bfeb338913f4571d3a12
David Daupeyroux <david.ddcreation@gmail.com>
2023-01-22 18:21:06 +0100

    test: :white_check_mark: Add app tests

--- jest.config.js
+++ jest.config.js
@@ -27,7 +27,12 @@ module.exports = {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/node_modules/', 'index.ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'index.ts',
+    '.interface.ts',
+    '.mock.ts',
+  ],
 
   // Indicates which provider should be used to instrument code for coverage
   // coverageProvider: "babel",
--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,4 +1,5 @@
 import { init } from './app';
+import { play, visualize } from './game-types';
 import { askForGameType } from './utils';
 
 jest.mock('./utils', () => ({
@@ -10,10 +11,53 @@ jest.mock('./game-types', () => ({
   visualize: jest.fn(),
 }));
 
+const mockedAskGameType = jest.mocked(askForGameType);
+const mockedVisualize = jest.mocked(visualize);
+const mockedPlay = jest.mocked(play);
+
 describe('Init', () => {
   it('Should ask for game type', async () => {
     await init();
 
-    expect(askForGameType).toHaveBeenCalledTimes(1);
+    expect(mockedAskGameType).toHaveBeenCalledTimes(1);
+  });
+
+  describe('When game type is visualize', () => {
+    beforeEach(() => {
+      mockedAskGameType.mockImplementationOnce(() =>
+        Promise.resolve('visualize')
+      );
+    });
+
+    it('Should call VISUALIZE', async () => {
+      await init();
+
+      expect(mockedVisualize).toHaveBeenCalled();
+    });
+  });
+
+  describe('When game type is PLAY', () => {
+    beforeEach(() => {
+      mockedAskGameType.mockImplementationOnce(() => Promise.resolve('play'));
+    });
+
+    it('Should call play', async () => {
+      await init();
+
+      expect(mockedPlay).toHaveBeenCalled();
+    });
+  });
+
+  describe('When NO game type is choosed', () => {
+    beforeEach(() => {
+      mockedAskGameType.mockImplementationOnce(() => Promise.resolve('aaaaaa'));
+    });
+
+    it('Should NOT call any game type', async () => {
+      await init();
+
+      expect(mockedVisualize).not.toHaveBeenCalled();
+      expect(mockedPlay).not.toHaveBeenCalled();
+    });
   });
 });
